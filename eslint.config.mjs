d39381f3e8d75b2f48f6d931bd5f2c24ae03a85e
thlam05 <108942151+thlam05@users.noreply.github.com>
2026-07-10 12:00:09 +0700

    add rules in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,11 +12,19 @@ export default defineConfig([
         ...globals.node,
         ...globals.jest,
       },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     rules: {
       "prefer-const": "error",
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "@typescript-eslint/no-explicit-any": "warn",
+      '@typescript-eslint/no-floating-promises': "error",
+      '@typescript-eslint/no-unsafe-argument': "warn",
+      '@typescript-eslint/no-unsafe-call': "error",
+      '@typescript-eslint/no-unsafe-member-access': "warn"
     },
   },
   eslintPluginPrettierRecommended,
